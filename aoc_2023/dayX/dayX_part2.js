636f53fa00ea9fdff1090a648caa9fa9487f7a49
DarioDeLuca <dar-de-lu@hotmail.it>
2023-12-03 15:08:22 +0100

    folder template

---
+++ aoc_2023/dayX/dayX_part2.js
@@ -0,0 +1,8 @@
+const { rowsFromTextFile } = require('../../generateArrayFromTextFile');
+
+function dayX_part2(){
+    const rows = rowsFromTextFile('aoc_2023/dayX/dayX_input.txt');
+    
+}
+
+module.exports = { dayX_part2 }
