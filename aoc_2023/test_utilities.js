c2f5783950268c0e45e153ccdacd7f06621b51ec
DarioDeLuca <dar-de-lu@hotmail.it>
2023-12-03 16:40:27 +0100

    file for correct answers and test function

---
+++ aoc_2023/test_utilities.js
@@ -0,0 +1,22 @@
+//correct results for my inputs
+const correct_answers = {
+  correct_result_day1_part1: 54081,
+  correct_result_day1_part2: 54649,
+  correct_result_day2_part1: 2727,
+  correct_result_day2_part2: 56580,
+  correct_result_day3_part1: 553079,
+  correct_result_day3_part2: 84363105,
+};
+
+function test(fnToTest, result, correct_result) {
+  if (result === correct_result) {
+    console.log("Test " + fnToTest + " passed. Sum is :", result);
+  } else {
+    console.error(
+      "Test " + fnToTest + " NOT passed. Sum is :",
+      result,
+      ". The sum is Expected to be " + correct_result
+    );
+  }
+}
+module.exports = { correct_answers, test };
