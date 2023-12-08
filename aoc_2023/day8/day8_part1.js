3fc5c3d0eed4884fc3c4ae6994ac3884da9a73e7
DarioDeLuca <dar-de-lu@hotmail.it>
2023-12-08 12:39:21 +0100

    solutions day 8

--- aoc_2023/day8/day8_part1.js
+++ aoc_2023/day8/day8_part1.js
@@ -1,10 +1,45 @@
 const { rowsFromTextFile } = require("../../generateArrayFromTextFile");
 
 function day8_part1() {
-  const rows = rowsFromTextFile("aoc_2023/day8/day8_input.txt");
-  console.log("rows", rows);
+  const rows = rowsFromTextFile("aoc_2023/day8/day8_input.txt").filter(r=>r)
+  const dir = rows.shift();
+  const nR = rows.map(r=>{
+    return r.substring(0,r.length-1)
+  });
+
+  const map=new Map();
+  nR.forEach(r=>{
+    let newR = r.split(' = (')
+   
+      map.set(newR[0],newR[1].split(', '))
+   
+  })
+
 
   let result = 0;
+  let find = false;
+  let start = 'AAA';
+  while(!find){
+    for(let i=0;i<dir.length;i++){
+      result++;
+      if(dir[i]==='R'){
+        start=map.get(start)[1];
+        if (start==='ZZZ') {
+          find=true;
+          break
+        };
+        continue;
+      }else{
+        start=map.get(start)[0];
+        
+        if (start==='ZZZ') {
+          find=true;
+          break
+        };
+      }
+    }
+  }
+
 
   return result;
 }
--- aoc_2023/day8/day8_part2.js
+++ aoc_2023/day8/day8_part2.js
@@ -1,13 +1,53 @@
 const { rowsFromTextFile } = require("../../generateArrayFromTextFile");
+const { lcm } = require('./utils');
+
 
 function day8_part2() {
-  const rows = rowsFromTextFile("aoc_2023/day8/day8_input.txt");
+  const rows = rowsFromTextFile("aoc_2023/day8/day8_input.txt").filter((r) => r);
+  const dir = rows.shift();
+  const nR = rows.map((r) => {
+    return r.substring(0, r.length - 1);
+  });
 
-  console.log("rows", rows);
+  const map = new Map();
+  nR.forEach((r) => {
+    let newR = r.split(" = (");
 
-  let result = 0;
+    map.set(newR[0], newR[1].split(", "));
 
-  return result;
+
+  });
+
+  function findZ(start, dir) {}
+
+  let result = [];
+  let next = Array.from(map.keys()).filter((k) => k[2] === "A");
+  for (ne of next) {
+    let p = ne;
+    let find = false;
+    let count =0;
+    while (!find) {
+      for (let i = 0; i < dir.length; i++) {
+        count++;
+        if (dir[i] === "R") {
+          p =  map.get(p)[1];
+          if (p[2] === "Z") {
+            find = true;
+            break;
+          }
+        } else {
+          p =  map.get(p)[0];
+          if (p[2] === "Z") {
+            find = true;
+            break;
+          }
+        }
+      }
+    }
+    result.push(count)
+  }
+
+  return lcm(result);
 }
 console.log(day8_part2());
 module.exports = { day8_part2 };
