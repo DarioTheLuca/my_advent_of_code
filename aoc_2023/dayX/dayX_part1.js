23130128fdf6ed61d60b3605dec345b320ccd5e8
DarioDeLuca <dar-de-lu@hotmail.it>
2023-12-08 16:00:42 +0100

    deleted folder with solutions in js

--- aoc_2023/dayX/dayX_part1.js
+++
@@ -1,8 +0,0 @@
-const { rowsFromTextFile } = require('../../generateArrayFromTextFile');
-
-function dayX_part1(){
-    const rows = rowsFromTextFile('aoc_2023/dayX/dayX_input.txt');
-    
-}
-
-module.exports = { dayX_part1 }
