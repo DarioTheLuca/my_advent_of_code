7a3ca6b2862e3ea31bffc2e582075c34006ce30f
DarioDeLuca <dar-de-lu@hotmail.it>
2023-12-08 17:36:52 +0100

    day 1 2022 solutions

---
+++ aoc2022/src/day01/index.ts
@@ -0,0 +1,91 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) =>
+  rawInput
+    .split("\n\n")
+    .map((i) => i.split("\n"))
+    .map((a) =>
+      a.reduce((acc, curr) => {
+        return acc + Number(curr);
+      }, 0),
+    );
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+
+  let max = 0;
+  for (const i of input) {
+    max = Math.max(max, i);
+  }
+
+  return max;
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  let top: number[] = [];
+  for (const i of input) {
+    if (top.length < 3) {
+      top.push(i);
+      top.sort((a, b) => a - b);
+    } else if (top.some((t) => i > t)) {
+      top.shift();
+      top.push(i);
+      top.sort((a, b) => a - b);
+    }
+  }
+  return top.reduce((acc, curr) => {
+    return acc + curr;
+  }, 0);
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `
+        1000
+        2000
+        3000
+
+        4000
+
+        5000
+        6000
+
+        7000
+        8000
+        9000
+
+        10000`,
+        expected: 24000,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `
+        1000
+        2000
+        3000
+
+        4000
+
+        5000
+        6000
+
+        7000
+        8000
+        9000
+
+        10000`,
+        expected: 45000,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
